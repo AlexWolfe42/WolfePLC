244afbd94d4b2dd501868214fcd02e4a4115361c
Alex Wolfe <hughalexwolfe42@gmail.com>
2020-11-14 20:02:50 -0600

    Initial Commit

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+/* Set email recipient */
+$myemail = "thewolfe42@gmail.com";
+
+/* Check all form inputs using check_input function */
+$firstname = check_input($_POST['first']);
+$lastname = check_input($_POST['last']);
+$email = check_input($_POST['email']);
+$message = check_input($_POST['message']);
+
+$subject = date("Y-m-d h:i:sa") 
+$emailmessage = "Steven,
+
+$first $last left you a message on your website.
+
+Their email is:  $email
+Message:
+$message
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
+
+/* Redirect visitor to the confirmation page */
+header('Location: contact_confirm.html');
+
+/* Functions */
+function check_input($data)
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	
+	return $data;
+}
+?>
